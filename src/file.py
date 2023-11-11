7d11b587d4b786561830da1ac2cf14064c990ae7
babireski <elian.babireski@gmail.com>
2023-11-11 11:04:30 -0300

    Equality defined for files

--- src/file.py
+++ src/file.py
@@ -3,3 +3,6 @@ class File:
         self.name = name
         self.extension = extension
         self.size = size
+
+    def __eq__(self, other):
+        return self.name == other.name and self.extension == other.extension
