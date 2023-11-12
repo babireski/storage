cfa07f96b99747181e9826986214df1142f4cfb4
babireski <elian.babireski@gmail.com>
2023-11-11 22:13:35 -0300

    Start server logic implemented

--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+import socket
+
 from storage import Storage
 
 class Server:
@@ -5,6 +7,12 @@ class Server:
         self.host = host
         self.port = port
         self.storage = Storage(path)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def start():
-        pass
+    def start(self):
+        try:
+            self.socket.bind((self.host, self.port))
+            self.socket.listen(5)
+            print("Server started on ", self.host, ":", self.port)
+        except socket.error as e:
+            print("Failed to start the server: ", e)
