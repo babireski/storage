e58f0770ce69f4ed9b82a1b69537d70b1ece0029
babireski <elian.babireski@gmail.com>
2023-11-29 08:49:47 -0300

    Storage logic implemented

--- src/file.py
+++ src/file.py
@@ -1,11 +1,12 @@
 class File:
-    def __init__(self, name, extension, size):
+    def __init__(self, name, extension, size, data):
         self.name = name
         self.extension = extension
         self.size = size
-
-    def __eq__(self, other):
-        return self.name == other.name and self.extension == other.extension
+        self.data = data
 
     def __str__(self):
         return self.name + "." + self.extension
+
+    def __eq__(self, other):
+        return self.name == other.name and self.extension == other.extension
--- src/storage.py
+++ src/storage.py
@@ -1,15 +1,32 @@
+import file
+import os
+
 class Storage:
-    def __init__(self, path):
+    def __init__(self, path, files):
         self.path = path
+        self.files = files
 
-    def list():
-        pass
+    def list(self):
+        # return list(map(lambda file: str(file), self.files))
+        return self.files
 
-    def upload():
-        pass
+    def upload(self, file):
+        filepath = self.path + file.name + '.' + file.extension if file.extension else self.path + file.name
+        with open(filepath, 'wb') as document:
+            document.write(file.data)
+        self.files.append(file)
 
     def download():
         pass
 
-    def delete():
-        pass
+    def delete(self, file):
+        if file in self.files:
+            filename = str(file)
+            filepath = self.path + filename
+            try:
+                os.remove(filepath)
+                self.files.remove(file)
+                return 'File {} removed successfully.'.format(filename)
+            except:
+                return 'The file {} does not exist in the repository.'.format(filename)
+        return 'The file {} was not found in the repository.'.format(filename)
