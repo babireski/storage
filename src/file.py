696ca36bd9a83b6494085eae4c80192d3938715d
babireski <elian.babireski@gmail.com>
2023-12-01 17:58:57 -0300

    Sender and receiver logic implemented

--- src/file.py
+++ src/file.py
@@ -10,3 +10,19 @@ class File:
 
     def __eq__(self, other):
         return self.name == other.name and self.extension == other.extension
+
+    def send(filepath, client):
+        with open(filepath, 'rb') as file:
+            data = file.read(1024)
+            while data:
+                client.send(data)
+                data = file.read(1024)
+        print("File sent successfully.")
+
+    def recv(filepath, client):
+        with open(filepath, 'wb') as file:
+            data = client.recv(1024)
+            while data:
+                file.write(data)
+                data = client.recv(1024)
+        print("File received successfully.")
